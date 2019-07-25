a3c4d95263538ba84e4cc1da1f384b6f7945b57d
Antonina Chernyshova <antonina@chernyshov.net>
2019-07-25 09:54:29 -0700

    Initial version of Twitter Data Gathering

---
+++ README.md
@@ -0,0 +1,4 @@
+# Twitter Data Gathering 
+
+Script for gathering tweets from Twitter API if we have a list of tweets ids
+Tweet Data Dictionary is here: https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
--- twitter_collect.py
+++ twitter_collect.py
@@ -4,17 +4,20 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy import API
 
-
 import datetime as dt
 import pandas as pd
 import os
 import csv
 
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = '**'
-ACCESS_SECRET = '**'
-CONSUMER_KEY = '**'
-CONSUMER_SECRET = '**'
+# Define the path
+ROOT_DIR = '/home/ubuntu/twitter/'
+
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = '***'
+ACCESS_SECRET = '***'
+CONSUMER_KEY = '***'
+CONSUMER_SECRET = '***'
 
 # Setup tweepy to authenticate with Twitter credentials:
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -23,8 +26,9 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 # Create the api to connect to twitter with your creadentials
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-
-def tweet_collection(json):
+#function accepts list of tweets ids and returns all tweets information as dictionary
+def tweet_collection(tweet_id):
+    json = api.get_status(tweet_id)._json
     tweet = {}
     tweet['id'] = json['id']
     tweet['created_at'] = json['created_at']
@@ -39,11 +43,9 @@ def tweet_collection(json):
     #tweet info
     tweet['retweet_count'] = json['retweet_count']
     tweet['favorite_count'] = json['favorite_count']
-    tweet['favorited'] = json['favorited']
-    tweet['retweeted'] = json['retweeted']
     tweet['lang'] = json['lang']
     tweet['is_quote_status'] = json['is_quote_status']
-    #about location if user uses geotags 
+    #about location if user uses geotags
     if json['place']!=None:
         tweet['place'] = json['place']
         tweet['place_name'] = json['place']['full_name']
@@ -53,32 +55,61 @@ def tweet_collection(json):
         tweet['coordinates_latitude'] = json['place']['bounding_box']['coordinates'][0][0][1]
     else:
         tweet['place']= None
-        tweet['place_name'] = None 
-        tweet['place_country'] = None 
-        tweet['coordinates'] = None 
-        tweet['coordinates_longitude'] = None 
-        tweet['coordinates_latitude'] = None 
+        tweet['place_name'] = None
+        tweet['place_country'] = None
+        tweet['coordinates'] = None
+        tweet['coordinates_longitude'] = None
+        tweet['coordinates_latitude'] = None
     return tweet
 
+#function accepts list of key words as an argument and returns new tweets with geo location and returns all tweets information as list of dictionaries
+def geo_tweets_collection(disaster_list):
+    result = []
+    for word in disaster_list:
+        for tweet in tweepy.Cursor(api.search, q=word, lang='en').items(100):
+            json = tweet._json
+            if json['place']!=None:
+                tweet = {}
+                tweet['id'] = json['id']
+                tweet['created_at'] = json['created_at']
+                tweet['text'] = json['text']
+                #information about user
+                tweet['user'] = json['user']
+                tweet['user_id'] = json['user']['id']
+                tweet['user_name'] = json['user']['name']
+                tweet['user_location'] = json['user']['location']
+                tweet['user_description'] = json['user']['description']
+                tweet['user_followers'] = json['user']['followers_count']
+                #tweet info
+                tweet['retweet_count'] = json['retweet_count']
+                tweet['favorite_count'] = json['favorite_count']
+                tweet['lang'] = json['lang']
+                tweet['is_quote_status'] = json['is_quote_status']
+                #about location if user uses geotags
+                tweet['place'] = json['place']
+                tweet['place_name'] = json['place']['full_name']
+                tweet['place_country'] = json['place']['country_code']
+                tweet['coordinates'] = json['place']['bounding_box']['coordinates'][0][0]
+                tweet['coordinates_longitude'] = json['place']['bounding_box']['coordinates'][0][0][0]
+                tweet['coordinates_latitude'] = json['place']['bounding_box']['coordinates'][0][0][1]
+                result.append(tweet)
+            else:
+                continue
+            time.sleep(3)
+    return result
 
-fields = ['id','created_at','text','user','user_id','user_name','user_location','user_description','user_followers',
-         'retweet_count','favorite_count','favorited','retweeted','lang','is_quote_status','place',
-         'place_name','place_country','coordinates','coordinates_longitude','coordinates_latitude']
-
-
-output = open('/home/ubuntu/twitter/tweets_results.csv','a+')  
-header = ",".join(fields)
-output.write(header + '\n')
-with open('/home/ubuntu/twitter/tweet_ids.txt','r') as file:
+output = open(ROOT_DIR + 'tweets_results.csv','a+') #create and open final file with results
+header = ",".join(list(tweet_collection(open(ROOT_DIR + 'tweet_ids.txt').readline()).keys())) #create a header
+output.write(header + '\n') #add header there
+with open(ROOT_DIR + 'tweet_ids.txt','r') as file: #loop through list of ids
     for tweet in file:
-        try: 
-            tweet_dict = tweet_collection(api.get_status(tweet)._json)
+        try:
+            tweet_dict = tweet_collection(tweet) #get data for selected tweet
             line = ",".join(["\"" + "".join(str(e).splitlines()).replace("\"", "") + "\"" for e in tweet_dict.values()])
-            output.write(line + '\n')
-            time.sleep(1)
+            output.write(line + '\n') #write it down
         except Exception as e:
-            f = open('/home/ubuntu/twitter/errors.txt','a+')
-            f.write(str(time.ctime()) +':' + str(e) + ' - ' + tweet)
-            time.sleep(1)
+            f = open(ROOT_DIR + 'errors.txt','a+') #save errors to errors.txt
+            f.write(str(time.ctime()) +':' + str(e) + tweet)
             f.close()
+    time.sleep(1) #add 1 sec to delay request in order to follow API rules (up to 900 requests per 15 minutes)
 output.close()
